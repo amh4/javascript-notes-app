b586bdcd27d27c048a87d56838acc6caf5dd7bd2
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-01-31 14:24:28 +0000

    Notes app display method changed so list does not compound

--- notesView.js
+++ notesView.js
@@ -1,24 +1,26 @@
 class NotesView{
   constructor(model){
-    // Inset Model into View so we can access the methods
     this.model = model;
-    // Get the element we want from the HTML and set it to a var
     this.mainContainerEl = document.querySelector('#main-container');
+    this.addButtonEl = document.querySelector('#add-note-button');
+
+
+    this.addButtonEl.addEventListener('click', () => {
+      const content = document.querySelector('#note-input').value;
+      this.model.addNote(content)
+      this.displayNotes();
+    })
   }
 
   displayNotes(){
-    // Get all the notes from the model class and assign to var
+    document.querySelectorAll('.note').forEach(element => {
+      element.remove();
+    })
     const notes = this.model.getNotes()
-
-    // Iterate through each note
     notes.forEach(note => {
-      // Create a new div for each note
       const noteEl = document.createElement('div');
-      // Set content to be the note
-      noteEl.textContent = note;
-      // Set all to the same class name
+      noteEl.innerText = note;
       noteEl.className = 'note';
-      // Append to the main HTML element
       this.mainContainerEl.append(noteEl);
     })
   }
--- notesView.test.js
+++ notesView.test.js
@@ -16,8 +16,39 @@
      model.addNote('Buy Milk')
      model.addNote('Buy Eggs')
 
-    view.displayNotes();
+     view.displayNotes();
  
-     expect(document.querySelectorAll('div.note').length).toBe(2);
+     expect(document.querySelectorAll('.note').length).toEqual(2);
    });
+
+   it('displays the added note', () => {
+     document.body.innerHTML = fs.readFileSync('./index.html');
+ 
+     const model = new NotesModel();
+     const view = new NotesView(model);
+
+     const inputEl = document.querySelector('#note-input');
+     inputEl.value = 'Test Value 1'
+
+     const buttonEl = document.querySelector('#add-note-button')
+     buttonEl.click();
+
+     expect(document.querySelector('.note').innerText).toEqual('Test Value 1');
+   });
+
+   it('displays the added notes', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const model = new NotesModel();
+    const view = new NotesView(model);
+
+    model.addNote('Buy Milk')
+    model.addNote('Buy Eggs')
+
+    view.displayNotes();
+    view.displayNotes();
+
+    expect(document.querySelectorAll('div.note').length).toEqual(2);
+  });
+   
  });
